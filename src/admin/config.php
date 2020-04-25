b966169543a6485668b29b89969be048d0ca66c3
Huy Nguyen Ngoc <romeohuy@gmail.com>
2020-04-25 11:17:49 +0700

    Setup local Eviroment

--- src/admin/config.php
+++ src/admin/config.php
@@ -1,14 +1,14 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'https://ankermart.com/admin/');
-define('HTTP_CATALOG', 'https://ankermart.com/');
+define('HTTP_SERVER', 'http://dev.ankermart.com/admin/');
+define('HTTP_CATALOG', 'http://dev.ankermart.com/');
 
 // HTTPS
-define('HTTPS_SERVER', 'https://ankermart.com/admin/');
-define('HTTPS_CATALOG', 'https://ankermart.com/');
+//define('HTTPS_SERVER', 'https://ankermart.com/admin/');
+//define('HTTPS_CATALOG', 'https://ankermart.com/');
 
 // DIR
-define('DIR_APPLICATION', '/home/ankepecl/public_html/admin/');
+/*define('DIR_APPLICATION', '/home/ankepecl/public_html/admin/');
 define('DIR_SYSTEM', '/home/ankepecl/public_html/system/');
 define('DIR_DATABASE', '/home/ankepecl/public_html/system/database/');
 define('DIR_LANGUAGE', '/home/ankepecl/public_html/admin/language/');
@@ -19,6 +19,19 @@ define('DIR_CACHE', '/home/ankepecl/public_html/system/cache/');
 define('DIR_DOWNLOAD', '/home/ankepecl/public_html/download/');
 define('DIR_LOGS', '/home/ankepecl/public_html/system/logs/');
 define('DIR_CATALOG', '/home/ankepecl/public_html/catalog/');
+*/
+
+define('DIR_APPLICATION', './admin/');
+define('DIR_SYSTEM', './system/');
+define('DIR_DATABASE', './system/database/');
+define('DIR_LANGUAGE', './admin/language/');
+define('DIR_TEMPLATE', './admin/view/template/');
+define('DIR_CONFIG', './system/config/');
+define('DIR_IMAGE', './image/');
+define('DIR_CACHE', './system/cache/');
+define('DIR_DOWNLOAD', './download/');
+define('DIR_LOGS', './system/logs/');
+define('DIR_CATALOG', './catalog/');
 
 // DB
 define('DB_DRIVER', 'mysqliz');
--- src/config.php
+++ src/config.php
@@ -1,12 +1,12 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'https://ankermart.com/');
+define('HTTP_SERVER', 'https://dev.ankermart.com/');
 
 // HTTPS
-define('HTTPS_SERVER', 'https://ankermart.com/');
+//define('HTTPS_SERVER', 'https://ankermart.com/');
 
 // DIR
-define('DIR_APPLICATION', '/home/ankepecl/public_html/catalog/');
+/*define('DIR_APPLICATION', '/home/ankepecl/public_html/catalog/');
 define('DIR_SYSTEM', '/home/ankepecl/public_html/system/');
 define('DIR_DATABASE', '/home/ankepecl/public_html/system/database/');
 define('DIR_LANGUAGE', '/home/ankepecl/public_html/catalog/language/');
@@ -16,6 +16,18 @@ define('DIR_IMAGE', '/home/ankepecl/public_html/image/');
 define('DIR_CACHE', '/home/ankepecl/public_html/system/cache/');
 define('DIR_DOWNLOAD', '/home/ankepecl/public_html/download/');
 define('DIR_LOGS', '/home/ankepecl/public_html/system/logs/');
+*/
+
+define('DIR_APPLICATION', './catalog/');
+define('DIR_SYSTEM', './system/');
+define('DIR_DATABASE', './system/database/');
+define('DIR_LANGUAGE', './catalog/language/');
+define('DIR_TEMPLATE', './catalog/view/theme/');
+define('DIR_CONFIG', './system/config/');
+define('DIR_IMAGE', './image/');
+define('DIR_CACHE', './system/cache/');
+define('DIR_DOWNLOAD', './download/');
+define('DIR_LOGS', './system/logs/');
 
 // DB
 define('DB_DRIVER', 'mysqliz');
